ea22240907936cdfbfed50979d3f2e00ac052be9
Filip Zyskowski <filipzyskowski@gmail.com>
2020-10-18 23:15:10 +0200

    add better animation to clicker

--- printable-calendar/src/components/NiceNextClicker.vue
+++ printable-calendar/src/components/NiceNextClicker.vue
@@ -2,29 +2,34 @@
     <div 
         ref="container" 
         :class="[clss, 'container-class']"
-        @mouseenter="debounceHoverOn()"
+        @mouseenter="hover = true"
         @mousemove="mouseMove"
-        @mouseleave="debounceHoverOff()"
+        @mouseleave="hover = false"
     >
-        <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" v-show="hover">
-            <path :d="getPath" stroke="transparent" fill="#eeeeee"/>
-        </svg>
-        <button 
-            v-show="hover" 
-            :class="buttonClass" 
-            :disabled="disabled" 
-            @click="$emit('click', $event)"
+        <transition-group 
+            name="fade"
+            
         >
-            <i :class="['mdi', 'fh-10', iconText]"></i>
-        </button>
+            <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" v-show="hover" key="1">
+                <path :d="getPath" stroke="transparent" fill="#eeeeee"/>
+            </svg>
+            <button 
+                v-show="hover" 
+                :class="buttonClass" 
+                :disabled="disabled" 
+                @click="$emit('click', $event)"
+                key="2"
+            >
+                <i :class="['mdi', 'fh-10', iconText]"></i>
+            </button>
+        </transition-group>
     </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from "vue-property-decorator";
-import { Orientation } from "../models/orientation"
-
-let timeout = 0;
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+import { Orientation } from "../models/orientation";
+import TWEEN from "@tweenjs/tween.js";
 
 @Component({})
 export default class NiceNextClicker extends Vue {
@@ -34,6 +39,7 @@ export default class NiceNextClicker extends Vue {
 
     hover = false;
     qy = 0;
+    qx = 0;
 
     get iconText() : string {
         return this.orientation === Orientation.Left ? 
@@ -58,43 +64,68 @@ export default class NiceNextClicker extends Vue {
         let endPart = ''
         if (this.orientation == Orientation.Left) {
             movePart += containerWidth + ',0';
-            bezierPart += '-10,' + this.Qy;
+            bezierPart += this.Qx + ',' + this.Qy;
             endPart += containerWidth + ',' + containerHeight;
         }
         else {
             movePart += '0,0';
-            bezierPart += (containerWidth + 10) + ',' + this.Qy;
+            bezierPart += this.Qx + ',' + this.Qy;
             endPart += '0,' + containerHeight;
         }
 
         return movePart + ' ' + bezierPart + ' ' + endPart;
     }
 
-    get Qy() : number {
+    get Qy(): number {
         return this.qy;
     }
     set Qy(qy: number) {
         this.qy = qy;
+    }
+    get Qx(): number {
+        return this.qx;
+    }
+    set Qx(qx: number) {
+        this.qx = qx;
     }
 
   mouseMove(event: MouseEvent) {
     this.Qy = event.clientY;
   }
 
-// debounce methods
-  debounceHover(func: Function) {
-    clearTimeout(timeout);
-    timeout = setTimeout(func, 100);
-  }
-  debounceHoverOn() {
-    this.debounceHover(() => {
-      this.hover = true;
-    });
-  }
-  debounceHoverOff() {
-    this.debounceHover(() => {
-      this.hover = false;
-    });
+  // watch
+  @Watch('hover')
+  onHoverChanged(val: boolean, oldVal: boolean) {
+    let containerWidth = 0;
+    if (this.$refs.container) {
+        containerWidth = (this.$refs.container as Element).clientWidth;
+    }   
+
+    let xTo = 0;
+    if (val) {
+        // show clicker
+        xTo = this.orientation == Orientation.Left ? -10 : containerWidth + 10;
+    }
+    else {
+        // hide clicker
+        xTo = this.orientation == Orientation.Left ? containerWidth : 0;
+    }
+    
+    function animate(time: number) {
+        if (TWEEN.update(time)) {
+            requestAnimationFrame(animate)
+        }
+    }
+
+    requestAnimationFrame(animate);
+
+    new TWEEN.Tween({x: this.Qx})
+    .to({x: xTo}, 500)
+    .easing(TWEEN.Easing.Quadratic.Out)
+    .onUpdate((prop) => {
+        this.Qx = prop.x;
+    })
+    .start(TWEEN.now());
   }
 }
 </script>
@@ -123,5 +154,15 @@ export default class NiceNextClicker extends Vue {
 
 .container-class {
     position: relative;
+}
+
+// cubic-bezier(0.470, 0.000, 0.745, 0.715)
+
+// animations 
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s
+}
+.fade-enter, .fade-leave-to {
+    opacity: 0
 }
 </style>
